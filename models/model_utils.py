04568145d3055058648b0a4e0728448d3717e996
kolhesamiksha <kolhesamiksha25@gmail.com>
2024-04-21 22:47:21 +0530

    feat:add security to api_key using pycryptodome AES algo

--- models/model_utils.py
+++ models/model_utils.py
@@ -9,9 +9,32 @@ import os
 from .data import load_and_transform_text, load_and_transform_vision_data, load_and_transform_audio_data
 from .imagebind_model import ModalityType, imagebind_huge
 
+import hashlib
+from Crypto.Cipher import AES 
+from Crypto.Util.Padding import pad, unpad 
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)  #override=True, always consider API_key from .env file
+
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # device= "cpu"
-ImageLike = Union["str", np.ndarray, Image.Image]
+ImageLike = Union['str', np.ndarray, Image.Image]
+
+def encrypt_pass(key):
+    random_input_text = b"Xysjgkyur8k4l8p5"
+    cipher = AES.new(random_input_text, AES.MODE_CBC)
+    key_bytes = key.encode('utf-8')
+    cipher_text = cipher.encrypt(pad(key_bytes, AES.block_size))
+
+    return cipher_text, cipher.iv
+# secure your API key with cryptography
+def decrypt_pass(encrypted_key, iv):
+    random_input_text = b"Xysjgkyur8k4l8p5"
+    iv = iv[:16]
+    cipher = AES.new(random_input_text, AES.MODE_CBC, iv)
+    plain_text = unpad(cipher.decrypt(encrypted_key), AES.block_size)
+    return plain_text.decode('utf-8')
 
 
 def get_model(dtype: torch.dtype = torch.float32) -> torch.nn.Module:
@@ -56,13 +79,13 @@ def get_embeddings(
     return embeddings
 
 def pinecone_retriever(query_embedding, pinecone_index_name, topk):
-
+    encrypted_key, iv = encrypt_pass(os.getenv("PINECONE_API_KEY"))
     pinecone = Pinecone(
-        api_key = os.environ['PINECONE_API_KEY']
+        api_key = decrypt_pass(encrypted_key, iv)
     )
 
     my_index_name = pinecone_index_name
     my_index = pinecone.Index(name = my_index_name)
 
-    result = my_index.query(vector=query_embedding.tolist()[0], top_k=topk, include_metadata=True)
+    result = my_index.query(vector=query_embedding.tolist(), top_k=topk, include_metadata=True)
     return result
